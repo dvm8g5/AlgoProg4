0b405338de6ae9c4c34ad3443ce982dd5485406f
Dan McConnell <dvm8g5@mst.edu>
2019-12-06 18:58:53 -0600

    Update Kruskal.py

--- Kruskal.py
+++ Kruskal.py
@@ -1,6 +1,11 @@
 import MyGraph
 
-def MST_Kruskal(Graph: [MyGraph.MyGraph], w):
-    a_forest = MyGraph.MyGraph()
-    for x in a_forest.vertices:
+
+def MST_Kruskal(Graph: MyGraph, w):
+    a_forest = Graph.adj_matrix
+    for x in a_forest.vertices:                 # Initializes the a_forest to have no connections, i.e. all are -1
+        for y in a_forest.vertices:
+            a_forest.adj_matrix[x][y] = -1
+
+    vertex_list = Graph.vertices
 
