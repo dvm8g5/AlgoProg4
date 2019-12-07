a614111235dd088ecb0ccdf2f4ea376f1b0b60a3
Ethan S <nerfsniper@att.net>
2019-12-06 22:49:53 -0600

    Very small fix

--- timing_framework.py
+++ timing_framework.py
@@ -21,7 +21,7 @@ class Timing:
         results = []
         for v in range(self.min_vertices, self.max_vertices):
             for e in range(self.min_edges, min((v ** 2 - v) // 2, self.max_edges)):
-                snippet = "MST_Kruskal(graph, graph.vertices)"
+                snippet = "MST_Kruskal(graph)"
                 init = "from Kruskal import MST_Kruskal\nfrom MyGraph import MyGraph\ngraph = MyGraph().init_random({},{})".format(v, e)
                 func_time = min(timeit.repeat(snippet, init, time.clock, 10, 25) / 25 * 10 ** 9)
                 results.append(TimingResult(v, e, func_time))
