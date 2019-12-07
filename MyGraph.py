a273bc012f541cee7a7835b45b90563ab7be79a0
Ethan S <nerfsniper@att.net>
2019-12-06 20:46:46 -0600

    Fixed graph generator to always produce single-tree graphs

--- MyGraph.py
+++ MyGraph.py
@@ -9,22 +9,32 @@ class MyGraph:
         self.vertices = vertices
 
     def init_random(self, vertex_count: int, edge_count: int):
-        if edge_count > vertex_count ** 2:
+        if edge_count > (vertex_count ** 2 - vertex_count) / 2:
+            print("Tried to create a graph with too many edges!")
+            return
+        if edge_count < vertex_count-1:
+            print("Tried to create a graph with too many vertices!")
             return
 
         self.adj_matrix = []
         self.add_vertices(vertex_count)
+        connected_verts = [0]  # The vertices that this graph connects
 
         for i in range(edge_count):
-            v1 = random.randint(0, vertex_count-1)
-            v2 = random.randint(0, vertex_count-1)
+            # Pick v1 from currently-spanned vertices
+            v1 = random.choice(connected_verts)
+            # Pick v2 from either non-spanned vertices, or a random vertex if all are spanned
+            v2 = random.choice([i for i in range(vertex_count) if i not in connected_verts or len(connected_verts) >= vertex_count])
             while v1 == v2 or self.adj_matrix[v1][v2] != 0 or self.adj_matrix[v2][v1] != 0:
-                v1 = random.randint(0, vertex_count-1)
-                v2 = random.randint(0, vertex_count-1)
+                v1 = random.randint(0, vertex_count)
+                v2 = random.randint(0, vertex_count)
 
             weight = random.randint(1, 10)
             self.add_edge(v1, v2, weight)
             self.add_edge(v2, v1, weight)
+
+            if v2 not in connected_verts:
+                connected_verts.append(v2)
 
     def clear_vertex_data(self):
         for v in self.vertices:
@@ -43,3 +53,11 @@ class MyGraph:
 
         for i in range(amount):           # Extend rows
             self.adj_matrix.append([0 for i in range(amount)])
+
+    def export_as_gv(self):
+        print("\ndigraph D {")
+        for i in range(len(self.adj_matrix)):
+            for j in range(i, len(self.adj_matrix[i])):
+                if self.adj_matrix[i][j] > 0:
+                    print("{} -> {}[label=\"{}\"]".format(i, j, self.adj_matrix[i][j]))
+        print("}\n")
