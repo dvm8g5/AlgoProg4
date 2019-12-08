c9637369ad028319696a1096d661a2016e4149e5
Ethan S <nerfsniper@att.net>
2019-12-07 23:12:22 -0600

    Update timing_framework.py

--- timing_framework.py
+++ timing_framework.py
@@ -21,8 +21,14 @@ class Timing:
         results = []
         for v in range(self.min_vertices, self.max_vertices):
             for e in range(self.min_edges, min((v ** 2 - v) // 2, self.max_edges)):
-                snippet = "MST_Kruskal(graph)"
-                init = "from Kruskal import MST_Kruskal\nfrom MyGraph import MyGraph\ngraph = MyGraph().init_random({},{})".format(v, e)
+                snippet = "test()"
+                init = """
+from Kruskal import MST_Kruskal
+from MyGraph import MyGraph
+graph = MyGraph().init_random({},{})
+def test():
+    MST_Kruskal(graph)
+                """.format(v, e)
                 func_time = min(timeit.repeat(snippet, init, time.clock, 10, 25) / 25 * 10 ** 9)
                 results.append(TimingResult(v, e, func_time))
 
@@ -32,8 +38,14 @@ class Timing:
         results = []
         for v in range(self.min_vertices, self.max_vertices):
             for e in range(self.min_edges, min((v ** 2 - v) // 2, self.max_edges)):
-                snippet = "MST_Prim(graph.vertices, graph.adj_matrix, 0"
-                init = "from Prims import PRIM\nfrom MyGraph import MyGraph\ngraph = MyGraph().init_random({},{})".format(v, e)
+                snippet = "test()"
+                init = """
+from Kruskal import MST_Kruskal
+from MyGraph import MyGraph
+graph = MyGraph().init_random({},{})
+def test():
+    MST_Kruskal(graph)
+                """.format(v, e)
                 func_time = min(timeit.repeat(snippet, init, time.clock, 10, 25) / 25 * 10 ** 9)
                 results.append(TimingResult(v, e, func_time))
 
